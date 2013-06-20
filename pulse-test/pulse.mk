3a56abc8db2aafce0f4e21ca2c028b5e8cded8f0
paul <wu_bao@sina.com>
2013-06-20 10:03:35 +0800

    add test_getopt.c

--- pulse-test/pulse.mk
+++ pulse-test/pulse.mk
@@ -2,6 +2,8 @@
 #TARGETS_C := $(TARGETS_DIR)/pulse-test
 TARGETS := $(TARGETS_DIR)/pulse-test
 
+LIB_VER := $(filter 3.%,$(shell uname -r))
+
 SRCS_C := $(wildcard *.c)
 OBJS_C := $(subst .c,.o,$(SRCS_C))
 OBJS_C := $(foreach i,$(OBJS_C),$(TARGETS_OBJ_DIR)/$(i))
@@ -9,7 +11,13 @@ OBJS_C := $(foreach i,$(OBJS_C),$(TARGETS_OBJ_DIR)/$(i))
 OBJS := $(OBJS_C) $(OBJS_CXX)
 
 #=========================================================================	
-LOCAL_LDLIBS := -lpulse -lpulsecommon-0.9.21 -lpulse-simple
+LOCAL_LDLIBS := -lpulse -lpulse-simple
+
+ifeq ($(LIB_VER),)
+LOCAL_LDLIBS += -lpulsecommon-0.9.21 
+else
+LOCAL_LDLIBS += -lpulsecommon-1.1 
+endif
 
 #=========================================================================	
 $(TARGETS): $(OBJS)
