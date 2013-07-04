a53a64227a5fbb541a4521681e07bd1580615e4a
paul <wu_bao@sina.com>
2013-07-04 10:07:16 +0800

    add test_so1

--- test_so1/test_so1.mk
+++ test_so1/test_so1.mk
@@ -1,39 +1,40 @@
 #=========================================================================
-#TARGETS_C := $(TARGETS_DIR)/pulse-test
-TARGETS := $(TARGETS_DIR)/test_so
-TARGETS_LIB := $(TARGETS_DIR)/libtest.so
-TARGETS_LIB1 := $(TARGETS_DIR)/libtest1.so
+TARGETS := $(TARGETS_DIR)/test_so1
+TARGETS_1 := $(TARGETS_DIR)/test_so1_1
+TARGETS_LIB := $(TARGETS_DIR)/libtest_so1.so
 
-SRCS_C := test_so.c
+SRCS_C := test_so1.c
 OBJS_C := $(subst .c,.o,$(SRCS_C))
 OBJS_C := $(foreach i,$(OBJS_C),$(TARGETS_OBJ_DIR)/$(i))
 OBJS := $(OBJS_C) $(OBJS_CXX)
 
-LIB_SRCS_CXX := libtest_so.cpp
-LIB_OBJS_CXX := $(subst .cpp,.o,$(LIB_SRCS_CXX))
-LIB_OBJS_CXX := $(foreach i,$(LIB_OBJS_CXX),$(TARGETS_OBJ_DIR)/$(i))
+SRCS1_C := test_so1_1.c
+OBJS1_C := $(subst .c,.o,$(SRCS1_C))
+OBJS1_C := $(foreach i,$(OBJS1_C),$(TARGETS_OBJ_DIR)/$(i))
+OBJS1 := $(OBJS1_C) $(OBJS1_CXX)
 
-LIB_SRCS_C := libtest1_so.c
+LIB_SRCS_C := libtest_so1.c
 LIB_OBJS_C := $(subst .c,.o,$(LIB_SRCS_C))
 LIB_OBJS_C := $(foreach i,$(LIB_OBJS_C),$(TARGETS_OBJ_DIR)/$(i))
 
 #=========================================================================	
 CFLAGS += -fPIC
 
-LOCAL_LDLIBS := -ldl
+LOCAL_LDLIBS := 
 LOCAL_LIB_LDLIBS :=
 
 #=========================================================================	
-$(TARGETS): $(TARGETS_LIB) $(TARGETS_LIB1) $(OBJS) 
-	@echo "[LD] $(TARGETS)"
-	$(_Q)$(CC) $(LDFLAGS) -o $@ $(OBJS) $(LOCAL_LDLIBS)	
-	
-$(TARGETS_LIB):	$(LIB_OBJS_CXX)
-	@echo "[LD] $(TARGETS_LIB)"
-	$(_Q)$(CXX) -shared $(LDFLAGS) -o $@ $^ $(LOCAL_LIB_LDLIBS)
+$(TARGETS): $(TARGETS_LIB) $(TARGETS_1) $(OBJS) 
+	@echo "[LD] $@"
+	$(_Q)$(CC) $(LDFLAGS) -o $@ $(OBJS) $(TARGETS_LIB) $(LOCAL_LDLIBS)	
 
-$(TARGETS_LIB1): $(LIB_OBJS_C)
-	@echo "[LD] $(TARGETS_LIB1)"
+$(TARGETS_1): $(OBJS1)
+	@echo "[LD] $@"
+	$(_Q)#$(CC) $(LDFLAGS) -o $@ $(OBJS1) $(TARGETS_LIB) $(LOCAL_LDLIBS)	
+	$(_Q)$(CC) $(LDFLAGS) -o $@ $(OBJS1) $(LOCAL_LDLIBS)	
+	
+$(TARGETS_LIB): $(LIB_OBJS_C)
+	@echo "[LD] $(TARGETS_LIB)"
 	$(_Q)$(CC) -shared $(LDFLAGS) -o $@ $^ $(LOCAL_LIB_LDLIBS)
 	
 #=========================================================================	
