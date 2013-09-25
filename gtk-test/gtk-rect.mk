dfe5a6dcb60da97e513b154b5020a0371f918cfb
paul <wu_bao@sina.com>
2013-09-25 14:39:50 +0800

    add intersect and gtk-test

---
+++ gtk-test/gtk-rect.mk
@@ -0,0 +1,21 @@
+#=========================================================================
+TARGETS := $(TARGETS_DIR)/gtk-rect
+
+SRCS_C := gtk-rect.c
+OBJS_C := $(subst .c,.o,$(SRCS_C))
+OBJS_C := $(foreach i,$(OBJS_C),$(TARGETS_OBJ_DIR)/$(i))
+
+OBJS := $(OBJS_C) $(OBJS_CXX)
+
+#=========================================================================	
+LOCAL_LDLIBS := 
+CFLAGS := `pkg-config --libs --cflags gtk+-2.0`
+LDFLAGS := `pkg-config --libs --cflags gtk+-2.0`
+
+#=========================================================================	
+$(TARGETS): $(OBJS)
+	@echo "[LD] $(TARGETS)"
+	$(_Q)$(CC) $(LDFLAGS) -o $@ $< $(LOCAL_LDLIBS)	
+	
+#=========================================================================	
+include $(TARGETS_ROOT)/mkconfig	
