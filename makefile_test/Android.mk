6f6613087cf04eb10c4881a6e7105792180d9de3
paul <wu_bao@sina.com>
2013-11-22 23:36:33 +0800

    makefile test 2

--- makefile_test/Android.mk
+++ makefile_test/Android.mk
@@ -3,8 +3,27 @@ my-dir = $(call parent-dir,$(lastword $(MAKEFILE_LIST)))
 all-makefiles-under = $(wildcard $1/*/Android.mk)
 all-subdir-makefiles = $(call all-makefiles-under, $(call my-dir))
 
+define debug_out
+@echo "======== `pwd`/$(lastword $(MAKEFILE_LIST)) ======="
+@echo "======== $(shell pwd)/$(lastword $(MAKEFILE_LIST)) ======="
+endef
+
+export debug_out
+export parent-dir
+export my-dir
+export all-makefiles-under
+export all-subdir-makefiles
+
 LOCAL_PATH := $(call my-dir)
 
-all:
+.PHONY: all clean
+
+
+all clean:
+	@$(call debug_out)
 	@echo "DIR = $(LOCAL_PATH)"
 	@echo $(all-subdir-makefiles)
+	@for file in $(all-subdir-makefiles); do \
+		echo "`dirname $$file` - `basename $$file`"; \
+		make -C `dirname $$file` -f `basename $$file` $(MAKECMDGOALS); \
+	done
--- makefile_test/mk1/Android.mk
+++ makefile_test/mk1/Android.mk
@@ -1,8 +1,7 @@
-parent-dir = $(patsubst %/,%,$(dir $1))
-my-dir = $(call parent-dir,$(lastword $(MAKEFILE_LIST)))
 
 LOCAL_PATH := $(call my-dir)
 
-#all:
-#	@echo $(MAKEFILE_LIST)
-#	@echo $(LOCAL_PATH)
+all:
+	@$(call debug_out)
+	@echo $(MAKEFILE_LIST)
+	@echo $(LOCAL_PATH)
