2eeed97dd45ec7c95aef521fc2f9305681b188e8
paul <wu_bao@sina.com>
2014-02-08 12:20:10 +0800

    add test sizeof pointer

--- test_sizeof.c
+++ test_sizeof.c
@@ -3,7 +3,7 @@
  * 		test_sizeof.c
  * 
  *  @note :
- * 		32bit和64bit的模式下, int, char的size不变， 只有long的size出现变化
+ * 		32bit和64bit的模式下, int(4), char(1)的size不变， 只有long(4 or 8)的size出现变化, 指针变量(4 or 8)也有变化
  *      
  **/
 #include <stdio.h>
@@ -28,11 +28,13 @@ int main(int argc, char *argv[])
 	int status;
 	long status1;
 	char buffer[128];
+	char *p;
 	
 	printf("sizeof(status) = %d, sizeof(int) = %d\n", (int)sizeof(status), (int)sizeof(int));
 	printf("sizeof(buffer) = %d, sizeof(char) = %d\n", (int)sizeof(buffer), (int)sizeof(char));
 	printf("sizeof(status1) = %d, sizeof(long) = %d\n", (int)sizeof(status1), (int)sizeof(long));	
 	printf("sizeof(test_st) = %d, sizeof(test1_st) = %d\n", (int)sizeof(test_st), (int)sizeof(test1_st));
+	printf("sizeof(p) = %d\n", (int)sizeof(p));
 			
 	return 0;
 }
