77946e0170cb289e92586d5e4fe9498f31073d83
paul <wu_bao@sina.com>
2014-09-12 15:51:16 +0800

    add shell strtok

--- shell/test_cmd_ret.sh
+++ shell/test_cmd_ret.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 #测试 &&, ||符号的使用
 #判断前面命令的输出是否有
@@ -6,8 +6,7 @@
 #   || 前面输出正常(返回0)会执行下一行命令，不执行||后面的命令；不正常，则执行后面的命令
 #
 
-function do_cmd1()
-{
+do_cmd1() {
 	echo "hello1"
 	return 0
 }
@@ -23,6 +22,31 @@ function do_cmd3()
 {
 	echo "hello3"
 	return
+}
+
+strtok() { # <string> { <variable> [<separator>] ... }
+	local tmp
+	local val="$1"
+	local count=0
+
+	shift
+
+	while [ $# -gt 1 ]; do
+		tmp="${val%%$2*}"
+
+		[ "$tmp" = "$val" ] && break
+
+		val="${val#$tmp$2}"
+
+		export ${NO_EXPORT:+-n} "$1=$tmp"; count=$((count+1))
+		shift 2
+	done
+
+	if [ $# -gt 0 -a -n "$val" ]; then
+		export ${NO_EXPORT:+-n} "$1=$val"; count=$((count+1))
+	fi
+
+	return $count
 }
 
 cmd1_ret=`do_cmd1`
@@ -44,3 +68,15 @@ cmd3_ret=`do_cmd3`
 echo "cmd3_ret = $cmd3_ret"
 is_cmd3_ok=`do_cmd3 > /dev/null && echo OK || echo FAIL`
 echo "$is_cmd3_ok"
+
+#
+strtok "hello world" str1 " "
+#return 1
+echo $?
+echo ${str1}
+#
+strtok "hello world" str1 " " str2 " "
+#return 2
+echo $?
+echo ${str1}
+echo ${str2}
