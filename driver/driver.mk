05ba9a8b07822e0ec9bc8b625a96d83bcc30f0ba
paul <wu_bao@sina.com>
2014-10-27 10:51:33 +0800

    add test mmap

--- driver/driver.mk
+++ driver/driver.mk
@@ -2,7 +2,7 @@
 MK_DIRS :=
 MK_DIRS += globalmem myfifo
 MK_DIRS += lddbus
-#MK_DIRS += sculld
+MK_DIRS += mymap
 
 #===============================================================================
 all_tests:
