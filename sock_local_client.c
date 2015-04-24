f8cd1e4470290cb49a6829638508bb4365e71d58
paul <wu_bao@sina.com>
2015-04-24 17:36:58 +0800

    add python readwave

--- sock_local_client.c
+++ sock_local_client.c
@@ -7,19 +7,36 @@
  **/
 //c_unix.c
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/wait.h>
 
 #define UNIX_DOMAIN "/tmp/UNIX.domain"
 
+static int connect_fd;
+
+static void signal_handler(int num)
+{
+    printf("Capture signal no:%d\n",num); 
+    if (connect_fd > 0) {
+		close(connect_fd);
+		connect_fd = 0;
+		printf("exit sock_local_client for signal\n");
+		exit(1);
+	}
+}
+     
 int main(void)
 {
-    int connect_fd;
     int ret;
     char snd_buf[1024];
     int i;
     static struct sockaddr_un srv_addr;
+    
+    signal(SIGPIPE, SIG_IGN); 
+    signal(SIGINT, signal_handler); 
     
 	//creat unix socket
     connect_fd = socket(PF_UNIX, SOCK_STREAM, 0);
@@ -41,11 +58,17 @@ int main(void)
     memset(snd_buf, 0, 1024);
     strcpy(snd_buf, "message from client");
 //send info server
-    for (i=0; i<4; i++) {
-        write(connect_fd, snd_buf, sizeof(snd_buf));
+    //for (i=0; i<4; i++) {
+    while (1) {
+		sleep(1);
+        ret = write(connect_fd, snd_buf, sizeof(snd_buf));
+        if (ret < 0) {
+			break;
+		}
 	}
 	
     close(connect_fd);
+    printf("exit sock_local_client\n");
     
     return 0;
 }
--- sock_local_server.c
+++ sock_local_server.c
@@ -7,9 +7,11 @@
  **/
 //s_unix.c
 #include <stdio.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h> 
+#include <sys/wait.h>
 
 #define UNIX_DOMAIN "/tmp/UNIX.domain"
 
@@ -24,6 +26,13 @@ int main(void)
     int len;
     struct sockaddr_un clt_addr;
     struct sockaddr_un srv_addr;
+    fd_set cur_rfds, glb_rfds;
+    int maxfd;
+    
+    FD_ZERO(&cur_rfds);
+    FD_ZERO(&glb_rfds);
+    
+    signal(SIGPIPE, SIG_IGN); 
     
     listen_fd = socket(PF_UNIX, SOCK_STREAM, 0);
     if (listen_fd < 0) {
@@ -53,26 +62,61 @@ int main(void)
         return 1;
     }
     
+    maxfd = listen_fd;
+    FD_SET(listen_fd, &glb_rfds);
+    com_fd = 0;
     //have connect request use accept
-    len = sizeof(clt_addr);
-    com_fd = accept(listen_fd, (struct sockaddr*)&clt_addr, &len);
-    if (com_fd<0) {
-        perror("cannot accept client connect request");
-        close(listen_fd);
-        unlink(UNIX_DOMAIN);
-        return 1;
-    }
-    //read and printf sent client info
-    printf("\n=====info=====\n");
-    for (i=0; i<4; i++) {
-        memset(recv_buf, 0, 1024);
-        int num = read(com_fd, recv_buf, sizeof(recv_buf));
-        printf("Message from client (%d)) :%s\n", num, recv_buf);  
-    }
-    
-    close(com_fd);
+    while (1) {
+       cur_rfds = glb_rfds;
+       if(select(maxfd+1, &cur_rfds, NULL, NULL, 0) < 0) {
+           perror("fail to select");
+           exit(1);
+       }
+		
+       if (FD_ISSET(listen_fd, &cur_rfds)) {
+			len = sizeof(clt_addr);
+			com_fd = accept(listen_fd, (struct sockaddr*)&clt_addr, &len);
+			if (com_fd<0) {
+				perror("cannot accept client connect request");
+				break;
+			}
+			
+			FD_SET(com_fd, &glb_rfds);
+			if (com_fd > maxfd) {
+				maxfd = com_fd;
+			}
+			printf("[s]input fd = %d\n", com_fd);
+		}
+		
+		if (com_fd > 0) {
+			if (FD_ISSET(com_fd, &cur_rfds)) {
+				//read and printf sent client info
+				printf("[s]\n=====info=====\n");
+				memset(recv_buf, 0, 1024);
+				int num = read(com_fd, recv_buf, sizeof(recv_buf));
+				if (num <= 0) {
+					printf("[s]error client %d\n", com_fd);
+					close(com_fd);
+					FD_CLR(com_fd, &glb_rfds);
+					com_fd = 0;
+					maxfd = listen_fd;					
+					//break;
+				}
+				
+				if (num > 0) {
+					printf("[s]Message from client (%d)) :%s\n", num, recv_buf);  
+				} 
+			}    			
+		}
+	}
+	
+	if (com_fd > 0) {
+		close(com_fd);
+	}
     close(listen_fd);
     unlink(UNIX_DOMAIN);
+    
+    printf("[s]exit sock_local_server\n");
     
     return 0;
 }
