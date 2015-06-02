52dc9618a9be340e9dce98c4ff7592f0c350eac8
paul <wu_bao@sina.com>
2015-06-02 09:43:56 +0800

    add test mspace

--- atomic/test_android_atomic.c
+++ atomic/test_android_atomic.c
@@ -11,13 +11,16 @@
 
 #include "atomic.h"
 
+#define TEST_THREADS	20
+
 static int count = 0;
+static int32_t flags = 0;
 
 void *test_func(void *arg)
 {
 	int i=0;
 	
-	for(i=0;i<20000;++i){
+	for(i=0; i<20000; ++i){
 	#if 1
 		android_atomic_add(1, &count);
 	#else
@@ -30,18 +33,24 @@ void *test_func(void *arg)
 
 int main(int argc, const char *argv[])
 {
-	pthread_t id[20];
+	pthread_t id[TEST_THREADS];
 	int i = 0;
-
-	for(i=0;i<20;++i){
+	int32_t ret;
+	
+	for(i=0; i<TEST_THREADS; ++i){
 		pthread_create(&id[i],NULL,test_func,NULL);
 	}
 
-	for(i=0;i<20;++i){
+	for(i=0; i<TEST_THREADS; ++i){
 		pthread_join(id[i],NULL);
 	}
 
 	printf("%d\n",count);
+	printf("flags = 0x%x\n", flags);
+	ret = android_atomic_or(0x01, &flags);
+	printf("android_atomic_or 0x01 flags = 0x%x, ret = 0x%x\n", flags, ret);
+	ret = android_atomic_and(~0x01, &flags);
+	printf("android_atomic_and ~0x01 flags = 0x%x, ret = 0x%x\n", flags, ret);
 	
 	return 0;
 }
